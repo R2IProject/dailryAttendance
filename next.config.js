25158302682b960e9492d38ee3b0c8091cfa26e0
R2|Project <afrili.rizki907@gmail.com>
2025-07-11 09:47:30 +0700

    security tweak

--- next.config.js
+++ next.config.js
@@ -1,6 +1,39 @@
-/** @type {import('next').NextConfig} */ // Add this JSDoc for type hinting
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+ async headers() {
+  return [
+   {
+    source: "/(.*)", // Applies headers to all routes
+    headers: [
+     {
+      key: "Strict-Transport-Security",
+      value: "max-age=63072000; includeSubDomains; preload",
+     },
+     {
+      key: "Content-Security-Policy",
+      value:
+       "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src *; font-src 'self'; connect-src *",
+     },
+     {
+      key: "X-Frame-Options",
+      value: "DENY",
+     },
+     {
+      key: "X-Content-Type-Options",
+      value: "nosniff",
+     },
+     {
+      key: "Referrer-Policy",
+      value: "no-referrer",
+     },
+     {
+      key: "Permissions-Policy",
+      value: "geolocation=(), camera=(), microphone=(), fullscreen=(self)",
+     },
+    ],
+   },
+  ];
+ },
 };
 
-module.exports = nextConfig; // CommonJS export
+module.exports = nextConfig;
